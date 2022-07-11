e3c608589b59420afce2aee9fe0300d327340cbb
Tatsuya28 <ioiodauphin880@gmail.com>
2022-07-11 17:02:25 +0200

    update CodinGame

---
+++ CodinGame/Easy/the-descent.py
@@ -0,0 +1,17 @@
+# The while loop represents the game.
+# Each iteration represents a turn of the game
+# where you are given inputs (the heights of the mountains)
+# and where you have to print an output (the index of the mountain to fire on)
+# The inputs you are given are automatically updated according to your last actions.
+
+# game loop
+while True:
+    max_height = 0
+    id_mountain = -1
+    for id in range(8):
+        mountain_h = int(input())  # represents the height of one mountain.
+        if mountain_h > max_height:
+            max_height, id_mountain = mountain_h, id
+
+    # The index of the mountain to fire on.
+    print(id_mountain)
